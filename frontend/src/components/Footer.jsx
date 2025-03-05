fec589e36b90d9d82603e749f03d6db74e5077cd
Arup <arup03.paul@gmail.com>
2025-03-05 18:54:54 +0530

    Adding Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-900 text-white text-center py-4 mb-0">
+            <p className="text-sm">&copy; 2025 Arup. All rights are reserved.</p>
+        </footer>
+
+    );
+};
+
+export default Footer;
